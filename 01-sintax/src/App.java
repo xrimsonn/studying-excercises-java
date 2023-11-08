4d35f316b877e233f6cdec1c4965c53649ed9dba
Antonio Rosales <jose.rosales.mald@gmail.com>
2023-11-07 19:59:39 -0600

    Main function

--- 01-sintax/src/App.java
+++ 01-sintax/src/App.java
@@ -1,5 +1,6 @@
 import utils.Fibonacci;
 import utils.Vowels;
+import utils.Numbers;
 import java.util.Scanner;
 import java.util.Arrays;
 
@@ -7,20 +8,33 @@ public class App {
   public static void main(String[] args) throws Exception {
     Scanner scanner = new Scanner(System.in);
     try {
-      System.out.print("Introduce un número: ");
+      System.out.print("Input a number: ");
       int number = scanner.nextInt();
-      System.out.println(Arrays.toString(Fibonacci.generateSequence(number)));
+      System.out.println("Fibonacci sequence: " + Arrays.toString(Fibonacci.generateSequence(number)));
     } catch (Exception e) {
-      System.err.println("Error: algo salio mal");
+      System.err.println("Error: something went wrong");
+      System.exit(0);
+    }
+
+    scanner.nextLine();
+
+    try {
+      System.out.print("Input a text: ");
+      String text = scanner.nextLine();
+      System.out.println("The text contains  " + Vowels.count(text).toString() + " vowels");
+    } catch (Exception e) {
+      System.err.println("Error: something went wrong");
+      System.exit(0);
     }
 
     try {
-      System.out.print("Introduce un texto: ");
-      scanner.nextLine();
-      String text = scanner.nextLine();
-      System.out.println("tiene " + Vowels.count(text).toString() + " vocales");
+      System.out.print("Input some numbers (separated by a coma): ");
+      int[] numbers = Arrays.stream(scanner.nextLine().split(","))
+                            .mapToInt(Integer::parseInt)
+                            .toArray();
+      System.out.println("Biggest number: " + Numbers.biggest(numbers).toString());
     } catch (Exception e) {
-      System.err.println("Error: algo salio mal");
+      System.err.println("Error: something went wrong");
       System.exit(0);
     }
     scanner.close();
